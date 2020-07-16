f21a30330bc1b25b6c1200fadb2fd5795e532300
houshixin <429436693@qq.com>
2020-07-16 09:40:21 +0800

    1

--- src/main/java/com/youjiuye/springdemo2/Springdemo2Application.java
+++ src/main/java/com/youjiuye/springdemo2/Springdemo2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springdemo2Application {
-
+//11
     public static void main(String[] args) {
         SpringApplication.run(Springdemo2Application.class, args);
     }
